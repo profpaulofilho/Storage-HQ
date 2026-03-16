8d6c2e9a8d4880913177e5383de802300a973ae1
profpaulofilho <profpaulofilho@gmail.com>
2026-03-16 19:34:13 -0300

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -598,7 +598,7 @@ def save_upload(file_storage, subdir='misc'):
     extension = filename.rsplit('.', 1)[1].lower()
     unique_name = f'{uuid4().hex}.{extension}'
 
-if drive_enabled():
+    if drive_enabled():
         folder_id = os.getenv('GOOGLE_DRIVE_FOLDER_ID')
 
         if not folder_id:
